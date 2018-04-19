f92b84ebaf1fbd8bcc7cb4b4b2a4c1b026857779
Atharva Kalsekar <kalsekar.atharva7@gmail.com>
2018-04-19 20:24:33 +0530

    organized work and added translations py

--- Transformations/translation.py
+++ Transformations/translation.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 
 img=cv2.imread('mark.jpg')
-width,height=img.shape[:2]
+cv2.imshow('Mark',img)
+height,width=img.shape[:2]
 q_width,q_height=width/4,height/4
-T= np.float32([[0,1,q_width],[1,0,q_height]])
+T = np.float32([[1,0,q_width],[0,1,q_height]])
+print (T)
 img_trans=cv2.warpAffine(img,T,(width,height))
 cv2.imshow('Translation',img_trans)
 cv2.waitKey(0)
