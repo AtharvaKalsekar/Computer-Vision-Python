2c7511c9fc5d4cd01c59f7c5488128b3ddfe6f0e
Atharva Kalsekar <kalsekar.atharva7@gmail.com>
2018-05-10 17:25:59 +0530

    cleaned the code of perspective transform

--- Transformations/perspectiveTransform.py
+++ Transformations/perspectiveTransform.py
@@ -29,19 +29,4 @@ cv2.setMouseCallback('Original',onClick)
 cv2.imshow('Original',img)
 cv2.waitKey()
 transform(img)
-
-#pts_A=np.float32([[320,25],[700,215],[85,610],[530,780]])
-'''pts_A=np.zeros((4,2))
-y=1
-for i in range(len(pts_A)):
-    pts_A[i]=onClick()
-    print(pts_A[i])
-#print(pts_A[1])
-#pts_B=np.float32([[0,0],[420,0],[0,594],[420,594]])
-#M=cv2.getPerspectiveTransform(pts_A,pts_B)
-#warped=cv2.warpPerspective(img,M,(420,594))
-#cv2.imshow('Original',img)
-#cv2.waitKey()
-#cv2.imshow('Warped',warped)
-cv2.waitKey()'''
 cv2.destroyAllWindows()
